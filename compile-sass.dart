0cf66a9f8ba8c037f779b9c1aab0fa1bd49ceed9
SkyWard <mail@skyward.pro>
2025-05-05 15:48:30 +0300

    fix: dart saas

--- compile-sass.dart
+++ compile-sass.dart
@@ -2,6 +2,18 @@ import 'dart:io';
 import 'package:sass/sass.dart' as sass;
 
 void main(List<String> arguments) {
-  var result = sass.compileToResult(arguments[0]);
-  new File(arguments[1]).writeAsStringSync(result.css);
+  if (arguments.length != 2) {
+    print('Usage: dart compile-sass.dart <input.scss> <output.css>');
+    exit(1);
+  }
+
+  try {
+    // New simplified compilation API
+    var compiledCss = sass.compile(arguments[0]);
+    File(arguments[1]).writeAsStringSync(compiledCss);
+    print('Successfully compiled ${arguments[0]} to ${arguments[1]}');
+  } catch (e) {
+    stderr.writeln('Error compiling Sass: $e');
+    exit(1);
+  }
 }
